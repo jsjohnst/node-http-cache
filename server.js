8c61f7b5692e230ec616fed303d3ba8446dcc66d
Marak Squires <marak.squires@gmail.com>
2010-09-18 07:25:37 -0400

    [api] httpCache.createServer is working now. getting close to 0.1.0 release

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ eyes = require('eyes'),
 http = require('http'),
 httpCache = require('./lib/http-cache');
 
-eyes.inspect(httpCache);
- // create a front-facing http server, this httpServer will be the first thing all incoming requests hit
- httpCache.createServer(9000, 'localhost').listen(8000);
+// create a front-facing http server, this httpServer will be the first thing all incoming requests hit
+httpCache.createServer(9000, 'localhost').listen(8000);
 
- http.createServer(function (req, res){
-   setTimeout(function(){
-     res.writeHead(200, {'Content-Type': 'text/plain'});
-     res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
-     res.end();
-   }, 3000); // this request will take SIX seconds to respond, intentional latency here to test cache
- }).listen(9000);
+// create a stub http server with latency around requests
+http.createServer(function (req, res){
+ setTimeout(function(){
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
+   res.end();
+ }, 3000); // this request will take 3 seconds to respond, intentional latency here to test cache
+}).listen(9000);
